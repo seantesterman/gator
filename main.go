ec77685c4800c498b0c42274f5bcfa75a1f1b33c
seantesterman <seantesterman@gmail.com>
2024-11-04 00:49:51 -0700

    5.0 RSS Feeds

--- main.go
+++ main.go
@@ -45,6 +45,7 @@ func main() {
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerUsers)
 	cmds.register("agg", handlerAgg)
+	cmds.register("addfeed", handlerAddFeed)
 
 	if len(os.Args) < 2 {
 		log.Fatalf("Usage: cli <command> [args...]")
