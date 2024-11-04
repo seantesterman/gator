683eee827b5ab6930a64d6f25cdfe69188be5232
seantesterman <seantesterman@gmail.com>
2024-11-04 01:59:04 -0700

    Unfollow

--- main.go
+++ main.go
@@ -48,6 +48,7 @@ func main() {
 	cmds.register("feeds", handlerListFeeds)
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("following", middlewareLoggedIn(handlerListFeedFollows))
+	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
 
 	if len(os.Args) < 2 {
 		log.Fatalf("Usage: cli <command> [args...]")
