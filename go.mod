4a2a85a149b6c6e7bc8d68211417e18774b3142b
seantesterman <seantesterman@gmail.com>
2024-11-03 21:52:12 -0700

    2.0 SQL Databases and Register command

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/seantesterman/gator
 
 go 1.23.1
+
+require github.com/lib/pq v1.10.9
+
+require github.com/google/uuid v1.6.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
 	"github.com/seantesterman/gator/internal/config"
+	"github.com/seantesterman/gator/internal/database"
 )
 
+// postgres://postgres:postgres@localhost:5432/gator?sslmode=disable
+
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -18,15 +24,24 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 	fmt.Printf("Read config: %+v\n", cfg)
+
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error opening postgres: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
-
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatalf("Usage: cli <command> [args...]")
